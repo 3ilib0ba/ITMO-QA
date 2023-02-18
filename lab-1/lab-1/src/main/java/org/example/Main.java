9c3bba08a258a7f7ff1b205c754ced44b93de900
3ilib0ba <evonniy@mail.ru>
2023-02-19 02:31:16 +0300

    task-2 done

--- lab-1/lab-1/src/main/java/org/example/Main.java
+++ lab-1/lab-1/src/main/java/org/example/Main.java
@@ -1,15 +1,32 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 import org.example.task1.CosFunction;
+import org.example.task2.CountSort;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Input an angle in rad: ");
-        Scanner input = new Scanner(System.in);
-        double x = input.nextDouble();
+        // task 1
+//        System.out.println("Input an angle in rad: ");
+//        Scanner input = new Scanner(System.in);
+//        double x = input.nextDouble();
+//        System.out.println("Result = " + CosFunction.cos(x));
 
-        System.out.println("Result = " + CosFunction.cos(x));
+
+        // task 2
+        int[] array = new int[]{10, 20, 14, 0, 2};
+        CountSort.sort(array, array.length);
+        System.out.println(Arrays.toString(array));
+
+
+
+
+
+
+
+        // task 3
+
     }
 }
