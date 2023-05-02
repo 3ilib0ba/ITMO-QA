7c534d5718f5baa43f65fe041f717b065f71a88d
3ilib0ba <evonniy@mail.ru>
2023-05-02 03:07:22 +0300

    ProfilePage done but editing profile page isn't working if it's not your profile

--- lab-3/src/test/java/com/example/lab3/pages/ProfilePage.java
+++ lab-3/src/test/java/com/example/lab3/pages/ProfilePage.java
@@ -3,6 +3,7 @@ package com.example.lab3.pages;
 import com.example.lab3.Utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ProfilePage extends Page {
     // HEADER
@@ -11,6 +12,16 @@ public class ProfilePage extends Page {
     private By notificationPageHrefBy = By.xpath("//*[@id=\"topMenu\"]/div[2]/section/nav/a[3]");
     private By friendsPageHrefBy = By.xpath("//*[@id=\"topMenu\"]/div[2]/section/nav/a[4]");
     private By profilePageHrefBy = By.xpath("//*[@id=\"topMenu\"]/div[2]/section/nav/a[5]/p");
+
+    private By askYourselfArea = By.xpath("//*[@id=\"question_question_text\"]");
+    private By askYourselfButton = By.xpath("//*[@id=\"profileAnswersForm\"]/div[2]/button");
+
+    // NAVIGATION (ANSWERS, QUESTIONS, VERSUSES)
+    private By answersBar = By.xpath("/html/body/main/main/div/section[2]/header/nav/a[1]");
+    private By questionsBar = By.xpath("/html/body/main/main/div/section[2]/header/nav/a[2]");
+    private By versusesBar = By.xpath("/html/body/main/main/div/section[2]/header/nav/a[3]");
+
+    private By editProfileHref = By.xpath("/html/body/main/main/aside[1]/section/h2[1]/a");
 
     private By usernameBy = By.xpath("/html/body/main/main/section[1]/header/div/div[1]/span");
     public ProfilePage(WebDriver driver) {
@@ -46,5 +57,35 @@ public class ProfilePage extends Page {
         Utils.getElementBySelector(driver, friendsPageHrefBy).click();
         Utils.waitUntilPageLoads(driver);
         return new FriendsPage(driver);
+    }
+
+    public void changeBarToAnswers() {
+        Utils.getElementBySelector(driver, answersBar).click();
+        Utils.waitUntilPageLoads(driver);
+    }
+
+    public void changeBarToQuestions() {
+        Utils.getElementBySelector(driver, questionsBar).click();
+        Utils.waitUntilPageLoads(driver);
+    }
+
+    public void changeBarToVersuses() {
+        Utils.getElementBySelector(driver, versusesBar).click();
+        Utils.waitUntilPageLoads(driver);
+    }
+
+    public void askYourself(String question) {
+        WebElement askingTextArea = Utils.getElementBySelector(driver, askYourselfArea);
+        askingTextArea.clear();
+        askingTextArea.sendKeys(question);
+
+        Utils.getElementBySelector(driver, askYourselfButton).click();
+        Utils.waitUntilPageLoads(driver);
+    }
+
+    public Page goToEditingProfilePage() {
+        Utils.getElementBySelector(driver, editProfileHref).click();
+        Utils.waitUntilPageLoads(driver);
+        return new Page(driver);
     }
 }
